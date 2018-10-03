e6958c7ab10ad644d3eb6284ef2c358d5ba0c822
ratexx <songpham@ais.co.th>
2018-10-03 11:57:06 +0700

    add write files

--- ihereapi.py
+++ ihereapi.py
@@ -1,8 +1,18 @@
 import webapp2
 class MainPage(webapp2.RequestHandler):
+    def write_file():
+        f = open("demofile.txt", "a")
+        f.write("Now the file has one more line!")
+
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
+        write_file()
+
+
+
+
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)
