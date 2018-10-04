2a517e820ad8611fc6a5a54df1214e3a4e833631
ratexx <ratxx@gmail.com>
2018-10-04 08:03:45 +0700

    new path

--- ihereapi.py
+++ ihereapi.py
@@ -1,24 +1,39 @@
 import webapp2
-import requests
 
-from requests.auth  import HTTPDigestAuth
-import json
 
-url ="http://iamhere-172709.appspot.com/pong/"
 
-#myRequest = requests.get(url,'')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 class MainPage(webapp2.RequestHandler):
-    def write_file(self):
-        f = open("demofile.txt", "a")
-        f.write("Now the file has one more line! \n")
-
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
-        write_file()
+        self.response.write('Hello, API!')
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -26,4 +41,3 @@ class MainPage(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/ihereapi/.*', MainPage),
 ], debug=True)
-
